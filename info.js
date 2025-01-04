045adb5984706b66b92af4897687014c961302d4
bloewe21 <gueegoy@gmail.com>
2025-01-03 19:48:53 -0800

    updated site link

--- info.js
+++ info.js
@@ -5,7 +5,7 @@ function shareFunction() {
     let titleText = title.textContent;
     let timeText = currentTimer.textContent;
     let foundText = "First three words: " + foundAnswers[0] + ", " + foundAnswers[1] + ", " + foundAnswers[2];
-    let siteText = "https://word-search-website.vercel.app/";
+    let siteText = "https://search-daily.vercel.app/";
     navigator.clipboard.writeText(titleText + "\n" + timeText + "\n" + foundText + "\n" + siteText);
 }
 
